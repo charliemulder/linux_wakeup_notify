a875a46e7587ecee49ebd5a3899f107fcb316524
charliemulder <charliemulder@users.noreply.github.com>
2022-09-19 17:03:05 +0800

    Update wake_up_bot.sh
    Modify content

--- wake_up_bot.sh
+++ wake_up_bot.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-TOKEN=xxxxxxxxx	#TG机器人token
-chat_ID=xxxxxxx		#用户ID或频道、群ID
-message_text="地震強度："$1+"          預計抵達時間: "$2		#要发送的信息
-MODE='HTML'		#解析模式，可选HTML或Markdown
-URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
-#测试1：终端有日志
+TOKEN=xxxxxxxxx	#TG bottoken
+chat_ID=xxxxxxx		#用户ID
+message_text="地震強度："$1+"          預計抵達時間: "$2		#Message want to send
+MODE='HTML'		#Using HTML as internet sample
+URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api
 curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}" 
