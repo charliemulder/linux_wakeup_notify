2bcb5813673cd5cadad0bfaa18abac10d6d63d13
charliemulder <charliemulder@users.noreply.github.com>
2022-09-19 21:31:50 +0800

    Update wake_up_bot.sh
    Add timestamp

--- wake_up_bot.sh
+++ wake_up_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TOKEN=xxxxxxxxx	#TG bottoken
 chat_ID=xxxxxxx		#用户ID
-message_text="地震強度："$1+"          預計抵達時間: "$2		#Message want to send
+message_text=$(date +%F_%T)+"->地震強度："$1+"          預計抵達時間: "$2		#Message want to send
 MODE='HTML'		#Using HTML as internet sample
 URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api
 curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}" 
